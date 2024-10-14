a1117b6acb78a2d2c570ef3dbd2468791063c51c
ProgrammablePotato <tamas.mihelik@gmail.com>
2024-10-14 10:47:33 +0200

    1

---
+++ src/main/java/lan/zold/CreateController.java
@@ -0,0 +1,44 @@
+package lan.zold;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CreateController {
+
+    EmployeeSource employeeSource;
+
+    @FXML
+    private TextField cityField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField salaryField;
+
+    @FXML
+    void onClickAddButton(ActionEvent event) {
+        startAddEmployee();
+        System.out.println("YEEEEEEEEHAAAAAAAW");
+    }
+
+    @FXML
+    void onClickExitButton(ActionEvent event) {
+        Platform.exit();
+    }
+
+    private void startAddEmployee() {
+        System.out.println("Hozzáadás indul...");
+        Employee emp = new Employee();
+        emp.setName(nameField.getText());
+        emp.setCity(cityField.getText());
+        String salaryStr = salaryField.getText();
+        Double salary = Double.parseDouble(salaryStr);
+        emp.setSalary(salary);
+        employeeSource = new EmployeeSource(new Sqlite());
+        employeeSource.addEmployee(emp);
+    }
+
+}
